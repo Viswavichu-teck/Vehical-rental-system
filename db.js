e674cfe884d2a188ea0140a7a33d4abe278b0445
Viswavichu-teck <viswaniti@gmail.com>
2024-09-07 22:28:43 +0530

    Car Rental

---
+++ db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+function connectDB(){
+
+    mongoose.connect('mongodb+srv://viswa:Vichu1907$$@cluster.wpw72ml.mongodb.net/vichucars' , {useUnifiedTopology: true , useNewUrlParser: true})
+
+    const connection = mongoose.connection
+
+    connection.on('connected' , ()=>{
+        console.log('Mongo DB Connection Successfull')
+    })
+
+    connection.on('error' , ()=>{
+        console.log('Mongo DB Connection Error')
+    })
+
+
+}
+
+connectDB()
+
+module.exports = mongoose
