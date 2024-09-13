af953834342e50a095d0b2fd03395f725baf65fa
Viswavichu-teck <viswaniti@gmail.com>
2024-09-13 12:26:13 +0530

    working

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
-const dbConnection = require('./db');
-app.use(express.json());
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const dbConnection = require('./db')
+app.use(express.json())
 
-app.use('/api/cars/', require('./routes/carsRoute'));
-app.use('/api/users/', require('./routes/usersRoute'));
-app.use('/api/bookings/', require('./routes/bookingsRoute'));
+app.use('/api/cars/' , require('./routes/carsRoute'))
+app.use('/api/users/' , require('./routes/usersRoute'))
+app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+const path = require('path')
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/api/users/register', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`));
-const cors = require('cors');
-app.use(cors({
-  origin: 'https://comfy-duckanoo-4c95de.netlify.app', // Your Netlify URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+if(process.env.NODE_ENV==='production')
+{
+
+    app.use('/' , express.static('client/build'))
+
+    app.get('*' , (req , res)=>{
+
+          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+
+    })
+
+}
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
