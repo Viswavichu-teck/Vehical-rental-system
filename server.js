9c987d470840feaff7ca633b82102f7a3a518a7b
Viswavichu-teck <viswaniti@gmail.com>
2024-09-18 13:36:15 +0530

    working

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
-const dbConnection = require('./db');  // Ensure DB connection is correct
-app.use(express.json());
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+const dbConnection = require('./db')
+app.use(express.json())
 
-app.use('/api/cars/', require('./routes/carsRoute'));
-app.use('/api/users/', require('./routes/usersRoute'));
-app.use('/api/bookings/', require('./routes/bookingsRoute'));
+app.use('/api/cars/' , require('./routes/carsRoute'))
+app.use('/api/users/' , require('./routes/usersRoute'))
+app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 
-const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve static files from the React app
-    app.use(express.static(path.join(__dirname, './client/build/index.html')));
+const path = require('path')
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-    });
+if(process.env.NODE_ENV==='production')
+{
+
+    app.use('/' , express.static('client/build'))
+
+    app.get('*' , (req , res)=>{
+
+          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+
+    })
+
 }
 
-app.listen(port, () => console.log(`Node JS Server Started on Port ${port}`));
+app.get('/', (req, res) => res.send('Hello World!'))
+
+
+ 
+
+
+app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
