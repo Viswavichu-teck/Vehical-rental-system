332aa42c05fbe41dbb8d6fbc6158b6c50181ed39
Viswavichu-teck <viswaniti@gmail.com>
2024-09-19 13:27:38 +0530

    working

--- server.js
+++ server.js
@@ -3,18 +3,16 @@ const app = express()
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
 app.use(express.json())
+const path = require('path');
 
 app.use('/api/cars/' , require('./routes/carsRoute'))
 app.use('/api/users/' , require('./routes/usersRoute'))
 app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 
-
-const path = require('path')
-
 if(process.env.NODE_ENV==='production')
 {
 
-    app.use('/' , express.static('client/build'))
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
 
     app.get('*' , (req , res)=>{
 
