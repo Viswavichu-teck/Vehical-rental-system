4f5f98fef04012f46e2cc6b75025614373fae26f
Viswavichu-teck <viswaniti@gmail.com>
2024-09-22 12:53:33 +0530

    car rental

--- server/server.js
+++ server/server.js
@@ -1,31 +1,24 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const dbConnection = require('./db')
-app.use(express.json())
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const dbConnection = require('./db');
 const path = require('path');
 
-app.use('/api/cars/' , require('./routes/carsRoute'))
-app.use('/api/users/' , require('./routes/usersRoute'))
-app.use('/api/bookings/' , require('./routes/bookingsRoute'))
+app.use(express.json());
 
-if(process.env.NODE_ENV==='production')
-{
+app.use('/api/cars/', require('./routes/carsRoute'));
+app.use('/api/users/', require('./routes/usersRoute'));
+app.use('/api/bookings/', require('./routes/bookingsRoute'));
 
-    app.use(express.static(path.join(__dirname, 'client', 'build')));
+if (process.env.NODE_ENV === 'production') {
+    // Adjust the path to point to the client build directory
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
-    app.get('*' , (req , res)=>{
-
-          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-
-    })
-
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+    });
 }
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
-
- 
-
-
-app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
+app.listen(port, () => console.log(`Node JS Server Started on Port ${port}`));
