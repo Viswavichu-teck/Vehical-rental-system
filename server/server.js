3425e600bdb3043797cee5150007489e551f338b
Viswavichu-teck <viswaniti@gmail.com>
2024-10-14 19:29:04 +0530

    Changed

--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
     });
 }
-
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Node JS Server Started on Port ${port}`));
